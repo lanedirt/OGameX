d263074791a2893b9ad01955d2580b4feb45bfc3
Chewbaka69 <3535011+Chewbaka69@users.noreply.github.com>
2024-06-27 11:03:53 +0200

    fix elseif

--- app/Factories/PlanetServiceFactory.php
+++ app/Factories/PlanetServiceFactory.php
@@ -135,9 +135,9 @@ class PlanetServiceFactory
     {
         if($coordinates->position >= 1 && $coordinates->position <= 3) { // Nearest planet from the sun
             return __('galaxy.planet.description.nearest');
-        } else if(($coordinates->position >= 4 && $coordinates->position <= 6) || ($coordinates->position >= 10 && $coordinates->position <= 12)) { // Normal planet
+        } elseif(($coordinates->position >= 4 && $coordinates->position <= 6) || ($coordinates->position >= 10 && $coordinates->position <= 12)) { // Normal planet
             return __('galaxy.planet.description.normal');
-        } else if($coordinates->position >= 7 && $coordinates->position <= 9) { // Biggest planet
+        } elseif($coordinates->position >= 7 && $coordinates->position <= 9) { // Biggest planet
             return __('galaxy.planet.description.biggest');
         } else /*($coordinates->position >= 13 && $coordinates->position <= 15)*/ { // Farthest planet from the sun
             return __('galaxy.planet.description.farthest');
