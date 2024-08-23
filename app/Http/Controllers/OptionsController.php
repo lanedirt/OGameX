a02c40667f365339e5973a0d1bc324a07ed7f300
Jeremy Baukens <jba@escaux.com>
2024-08-23 12:02:34 +0200

    apply Laravel Pint PSR-12 coding standards

--- app/Http/Controllers/OptionsController.php
+++ app/Http/Controllers/OptionsController.php
@@ -55,7 +55,7 @@ class OptionsController extends OGameController
             $player->setUsername($name);
             $player->save();
         }
- 
+
         return array('success' => __('Settings saved'));
     }
 
