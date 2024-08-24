3bd8e4ec68c60de8da602ff428a8a09d8365267a
Jeremy Baukens <jba@escaux.com>
2024-08-24 18:54:48 +0200

    Implement notes popup logic

---
+++ app/Services/NoteService.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace OGame\Services;
+
+use OGame\Models\Note;
+
+/**
+ * Class NoteService.
+ *
+ * NoteService object.
+ *
+ * @package OGame\Services
+ */
+class NoteService
+{
+    /**
+     * The PlayerService object.
+     *
+     * @var PlayerService
+     */
+    private PlayerService $player;
+
+    /**
+     * NoteService constructor.
+     */
+    public function __construct(PlayerService $player)
+    {
+        $this->player = $player;
+    }
+
+    /**
+     * Create a new note for a player.
+     *
+     * @param array<string, mixed> $data
+     * @return Note
+     */
+    public function createNoteForUser(array $data): Note
+    {
+        $note = new Note();
+        $note->user_id = $this->player->getId();
+        $note->priority = $data['priority'];
+        $note->subject = $data['subject'];
+        $note->content = $data['content'];
+        $note->save();
+
+        return $note;
+    }
+
+    /**
+     * Retrieve all notes for the current player.
+     *
+     * @return Note[] Array of Note objects.
+     */
+    public function getAllNotesForUser(): array
+    {
+        return Note::where('user_id', $this->player->getId())->get()->all();
+    }
+
+    /**
+     * Retrieve a specific note for the current user.
+     *
+     * @param int $noteId
+     * @return Note|null
+     */
+    public function getNoteById(int $noteId): ?Note
+    {
+        return Note::where('id', $noteId)
+            ->where('user_id', $this->player->getId())
+            ->first();
+    }
+
+    /**
+     * Update a specific note for the current user.
+     *
+     * @param int $noteId
+     * @param array<string, mixed> $data
+     * @return Note
+     */
+    public function updateNoteForUser(int $noteId, array $data): Note
+    {
+        $note = Note::where('id', $noteId)
+            ->where('user_id', $this->player->getId())
+            ->first();
+
+        $note->update($data);
+
+        return $note;
+    }
+
+    /**
+     * Deletes all the notes.
+     *
+     * @return void
+     */
+    public function deleteAllNotesForUser(): void
+    {
+        Note::where('user_id', $this->player->getId())
+            ->delete();
+    }
+
+    /**
+     * Deletes marked notes.
+     *
+     * @param int[] $noteIds
+     * @return void
+     */
+    public function deleteMarkedNotes(array $noteIds): void
+    {
+        Note::whereIn('id', $noteIds)
+            ->where('user_id', $this->player->getId())
+            ->delete();
+    }
+
+    /**
+     * Check if a note exists and belongs to the current user.
+     *
+     * @param int $noteId
+     * @return bool
+     */
+    public function noteExistsAndBelongsToUser(int $noteId): bool
+    {
+        return Note::where('id', $noteId)
+            ->where('user_id', $this->player->getId())
+            ->exists();
+    }
+}
--- resources/views/ingame/notes/overlay.blade.php
+++ resources/views/ingame/notes/overlay.blade.php
@@ -1,46 +1,63 @@
-<div id="noteList" rel="#">
-    <a href="#" class="btn_blue openOverlay" data-title="Add note" data-overlay-class="newNote-1" id="newNote">
-        <span>+ New Note</span>
-    </a>
+<div id="noteList" rel="{{ route('notes.overlay') }}">
+    <a href="{{ route('notes.view') }}" class="btn_blue openOverlay" data-title="@lang('Add note')" data-overlay-class="newNote-1" id="newNote">
+        <span>+ @lang('New Note')</span>
+    </a> 
     <form action="javascript:void(0);" method="post">
-        <input type="hidden" name="token" value="b62002b60cc674937a9e5e0471e594c0">
+        <input type='hidden' name='_token' value='{{ csrf_token() }}' />
         <table cellspacing="0" cellpadding="0" id="notizen">
             <thead>
             <tr>
                 <th class="spacer">
-                </th><th class="subject">Subject</th>
-                <th class="date">Date</th>
+                </th><th class="subject">@lang('Subject')</th>
+                <th class="date">@lang('Date')</th>
             </tr>
             </thead>
             <tbody>
-            <tr class="alt">
-                <td width="30"><input type="checkbox" value="314" name="delIds[]"></td>
-                <td class="subject">
-                    <a href="#" class="openOverlay" data-title="Edit note" data-overlay-class="note-314">
-                        <span class="undermark">attack pages</span>
-                    </a>
-                </td>
-                <td class="date">18.03.2024 22:00:35</td>
-            </tr>
+                @if(count($notes) > 0)
+                    @foreach($notes as $note)
+                        <tr class="alt">
+                            <td width="30">
+                                <input type="checkbox" value="{{ $note->id }}" name="delIds[]">
+                            </td>
+                            <td class="subject">
+                                <a href="{{ route('notes.view', ['id' => $note->id]) }}" class="openOverlay" data-title="@lang('Edit note')" data-overlay-class="note-{{ $note->id }}">
+                                    <span class="{{ $note->priority === 1 ? 'overmark' : ($note->priority === 2 ? 'undermark' : '') }}">{{ $note->subject }}</span>
+                                </a>
+                            </td>
+                            <td class="date">{{ $note->created_at->format('d.m.Y H:i:s') }}</td>
+                        </tr>
+                    @endforeach
+                @else
+                    <tr class="alt">
+                        <td colspan="4" align="center">No notes found</td>
+                    </tr>
+                @endif
             </tbody>
+            @if(count($notes) > 0)
             <tfoot>
-            <tr class="last-tr">
-                <td colspan="2" class="options">
-                    <select name="noticeDeleteMethode" class="choose dropdownInitialized" style="display: none;">
-                        <option value="0">Select action</option>
-                        <option value="1">Delete marked notes</option>
-                        <option value="2">Delete all notes</option>
-                    </select><span class="dropdown currentlySelected choose" rel="dropdown183" style="width: 250px;"><a class="undefined" data-value="0" rel="dropdown183" href="javascript:void(0);">Select action</a></span>
-                    <input type="submit" value="Ok" name="delNow" class="btn_blue buttonOK">
-                </td>
-                <td align="right">
-                </td>
-            </tr>
+                <tr class="last-tr">
+                    <td colspan="2" class="options">
+                        <select name="noticeDeleteMethode" class="choose">
+                            <option value="0">@lang('Select action')</option>
+                            <option value="1">@lang('Delete marked notes')</option>
+                            <option value="2">@lang('Delete all notes')</option>
+                        </select>
+                        <input type="submit" value="Ok" name="delNow" class="btn_blue buttonOK">
+                    </td>
+                    <td align="right">
+                    </td>
+                </tr>
             </tfoot>
+            @endif
         </table>
     </form>
 </div>
 <script type="text/javascript">
-    var locaNotes = {"changesNotSaved":"The message has not been saved. All changes will be lost if you leave the page.","questionSaveChanges":"Should the changes be saved?"};
+    var locaNotes = {"changesNotSaved":"@lang('The message has not been saved. All changes will be lost if you leave the page.')","questionSaveChanges":"@lang('Should the changes be saved?')"};
     initNotes();
+    @if (isset($success) && !empty($success))
+    $(document).ready(function () {
+        fadeBox("{!! $success !!}", 0);
+    });
+    @endif
 </script>
