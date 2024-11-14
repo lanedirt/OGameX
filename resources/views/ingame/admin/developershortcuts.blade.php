bcf67ff6ea57841efd1d3cb8c2fb21f825780b79
Leendert de Borst <ldeborst@xivisoft.com>
2024-11-14 22:58:15 +0100

    Update developer shortcuts template to make all text translatable

--- resources/views/ingame/admin/developershortcuts.blade.php
+++ resources/views/ingame/admin/developershortcuts.blade.php
@@ -12,12 +12,12 @@
 
     <div id="resourcesettingscomponent" class="maincontent">
         <div id="planet" class="shortHeader">
-            <h2>Developer shortcuts</h2>
+            <h2>@lang('Developer shortcuts')</h2>
         </div>
 
         <div id="buttonz">
             <div class="header">
-                <h2>Developer shortcuts</h2>
+                <h2>@lang('Developer shortcuts')</h2>
             </div>
             <div class="content">
                 <div class="buddylistContent">
@@ -26,17 +26,17 @@
                                 <p class="box_highlight textCenter no_buddies">@lang('Update current planet:')</p>
                                 <div class="group bborder" style="display: block;">
                                     <div class="fieldwrapper">
-                                        <input type="submit" class="btn_blue" name="set_mines" value="Set all mines to level 30">
-                                        <input type="submit" class="btn_blue" name="set_storages" value="Set all storages to level 15">
-                                        <input type="submit" class="btn_blue" name="set_shipyard" value="Set all shipyard facilities to level 12">
-                                        <input type="submit" class="btn_blue" name="set_research" value="Set all research to level 10">
+                                        <input type="submit" class="btn_blue" name="set_mines" value="@lang('Set all mines to level 30')">
+                                        <input type="submit" class="btn_blue" name="set_storages" value="@lang('Set all storages to level 15')">
+                                        <input type="submit" class="btn_blue" name="set_shipyard" value="@lang('Set all shipyard facilities to level 12')">
+                                        <input type="submit" class="btn_blue" name="set_research" value="@lang('Set all research to level 10')">
                                     </div>
                                 </div>
 
                                 <p class="box_highlight textCenter no_buddies">@lang('Add X of unit to current planet:')</p>
                                 <div class="group bborder" style="display: block;">
                                     <div class="fieldwrapper">
-                                        <label class="styled textBeefy">Amount of units to add:</label>
+                                        <label class="styled textBeefy">@lang('Amount of units to add:')</label>
                                         <div class="thefield">
                                             <input type="text" pattern="^[0-9,.kmb]+$" class="textInput w50 textCenter textBeefy" value="1" size="2" name="amount_of_units">
                                         </div>
@@ -46,17 +46,17 @@
                                         @foreach ($units as $unit)
                                             <input type="submit" name="unit_{{ $unit->id }}" class="btn_blue" value="{{ $unit->title }}">
                                         @endforeach
-                                        <input type="submit" class="btn_blue" value="Light fighter">
+                                        <input type="submit" class="btn_blue" value="@lang('Light fighter')">
                                     </div>
                                 </div>
 
                                 <p class="box_highlight textCenter no_buddies">@lang('Reset planet')</p>
                                 <div class="group bborder" style="display: block;">
                                     <div class="fieldwrapper">
-                                        <input type="submit" class="btn_blue" name="reset_buildings" value="Set all buildings to level 0">
-                                        <input type="submit" class="btn_blue" name="reset_research" value="Set all research to level 0">
-                                        <input type="submit" class="btn_blue" name="reset_units" value="Remove all units">
-                                        <input type="submit" class="btn_blue" name="reset_resources" value="Set all resources to 0">
+                                        <input type="submit" class="btn_blue" name="reset_buildings" value="@lang('Set all buildings to level 0')">
+                                        <input type="submit" class="btn_blue" name="reset_research" value="@lang('Set all research to level 0')">
+                                        <input type="submit" class="btn_blue" name="reset_units" value="@lang('Remove all units')">
+                                        <input type="submit" class="btn_blue" name="reset_resources" value="@lang('Set all resources to 0')">
                                     </div>
                                 </div>
                             </form>
@@ -66,8 +66,8 @@
                                 <p class="box_highlight textCenter no_buddies">@lang('Add / subtract X of resource to current planet:')</p>
                                 <div class="group bborder" style="display: block;">
                                     <div class="fieldwrapper">
-                                        <div class="smallFont">You can enter positive or negative values to add or subtract to the selected resource. Supports k/m/b suffixes (e.g., 1k, 2m, 3b)</div>
-                                        <label class="styled textBeefy">Resources to add/subtract:</label>
+                                        <div class="smallFont">@lang('You can enter positive or negative values to add or subtract to the selected resource. Supports k/m/b suffixes (e.g., 1k, 2m, 3b)')</div>
+                                        <label class="styled textBeefy">@lang('Resources to add/subtract:')</label>
                                         <div class="thefield" style="display: flex; flex-direction: column; gap: 10px;">
                                             @foreach (\OGame\Models\Enums\ResourceType::cases() as $resource)
                                                 <div style="display: flex; gap: 10px;">
@@ -90,29 +90,29 @@
                                 <p class="box_highlight textCenter no_buddies">@lang('Create planet/moon at coordinates:')</p>
                                 <div class="group bborder" style="display: block;">
                                     <div class="fieldwrapper">
-                                        <label class="styled textBeefy">Coordinates:</label>
+                                        <label class="styled textBeefy">@lang('Coordinates:')</label>
                                         <div class="thefield" style="display: flex; gap: 10px;">
                                             <div>
-                                                <label for="galaxy">Galaxy:</label>
+                                                <label for="galaxy">@lang('Galaxy:')</label>
                                                 <input type="text" id="galaxy" pattern="^[-+0-9,.kmb]+$" class="textInput w50 textCenter textBeefy"
                                                        value="{{ $currentPlanet->getPlanetCoordinates()->galaxy }}" min="1" max="6" name="galaxy">
                                             </div>
                                             <div>
-                                                <label for="system">System:</label>
+                                                <label for="system">@lang('System:')</label>
                                                 <input type="text" id="system" pattern="^[-+0-9,.kmb]+$" class="textInput w50 textCenter textBeefy"
                                                        value="{{ $currentPlanet->getPlanetCoordinates()->system }}" min="1" max="499" name="system">
                                             </div>
                                             <div>
-                                                <label for="position">Position:</label>
+                                                <label for="position">@lang('Position:')</label>
                                                 <input type="text" id="position" pattern="^[-+0-9,.kmb]+$" class="textInput w50 textCenter textBeefy"
                                                        value="{{ $currentPlanet->getPlanetCoordinates()->position }}" min="1" max="15" name="position">
                                             </div>
                                         </div>
                                     </div>
                                     <div class="fieldwrapper" style="text-align: center; margin-bottom: 20px;">
-                                        <input type="submit" class="btn_blue" name="create_planet" value="Create Planet (For Myself)">
-                                        <input type="submit" class="btn_blue" name="create_moon" value="Create Moon (For Planet Owner)">
-                                        <input type="submit" class="btn_blue" name="delete_moon" value="Delete Moon">
+                                        <input type="submit" class="btn_blue" name="create_planet" value="@lang('Create Planet (For Myself)')">
+                                        <input type="submit" class="btn_blue" name="create_moon" value="@lang('Create Moon (For Planet Owner)')">
+                                        <input type="submit" class="btn_blue" name="delete_moon" value="@lang('Delete Moon')">
                                     </div>
                                 </div>
                             </form>
@@ -122,27 +122,27 @@
                                 <p class="box_highlight textCenter no_buddies">@lang('Create/delete debris field at coordinates:')</p>
                                 <div class="group bborder" style="display: block;">
                                     <div class="fieldwrapper">
-                                        <label class="styled textBeefy">Coordinates:</label>
+                                        <label class="styled textBeefy">@lang('Coordinates:')</label>
                                         <div class="thefield" style="display: flex; gap: 10px;">
                                             <div>
-                                                <label for="galaxy">Galaxy:</label>
+                                                <label for="galaxy">@lang('Galaxy:')</label>
                                                 <input type="text" id="galaxy" pattern="^[-+0-9,.kmb]+$" class="textInput w50 textCenter textBeefy"
                                                        value="{{ $currentPlanet->getPlanetCoordinates()->galaxy }}" min="1" max="6" name="galaxy">
                                             </div>
                                             <div>
-                                                <label for="system">System:</label>
+                                                <label for="system">@lang('System:')</label>
                                                 <input type="text" id="system" pattern="^[-+0-9,.kmb]+$" class="textInput w50 textCenter textBeefy"
                                                        value="{{ $currentPlanet->getPlanetCoordinates()->system }}" min="1" max="499" name="system">
                                             </div>
                                             <div>
-                                                <label for="position">Position:</label>
+                                                <label for="position">@lang('Position:')</label>
                                                 <input type="text" id="position" pattern="^[-+0-9,.kmb]+$" class="textInput w50 textCenter textBeefy"
                                                        value="{{ $currentPlanet->getPlanetCoordinates()->position }}" min="1" max="15" name="position">
                                             </div>
                                         </div>
                                     </div>
                                     <div class="fieldwrapper">
-                                        <label class="styled textBeefy">Resources to add:</label>
+                                        <label class="styled textBeefy">@lang('Resources to add:')</label>
                                         <div class="thefield" style="display: flex; flex-direction: column; gap: 10px;">
                                             @foreach (\OGame\Models\Enums\ResourceType::cases() as $resource)
                                                 <div style="display: flex; gap: 10px;">
@@ -155,8 +155,8 @@
                                         </div>
                                     </div>
                                     <div class="fieldwrapper" style="text-align: center; margin-bottom: 50px;">
-                                        <input type="submit" class="btn_blue" name="create_debris" value="Create/Append Debris Field">
-                                        <input type="submit" class="btn_blue" name="delete_debris" value="Delete Debris Field">
+                                        <input type="submit" class="btn_blue" name="create_debris" value="@lang('Create/Append Debris Field')">
+                                        <input type="submit" class="btn_blue" name="delete_debris" value="@lang('Delete Debris Field')">
                                     </div>
                                 </div>
                             </form>
