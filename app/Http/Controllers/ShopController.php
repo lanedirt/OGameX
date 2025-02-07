6614f124c72283c0b333748df1ec4437c2d0f230
Ryan Sandnes <theshado40@hotmail.com>
2025-02-06 16:32:55 -0800

    Fixed bug #418, Shop Layout portion
    - Added ShopPageTest
    - setBodyId to 'shop' in ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -13,6 +13,9 @@ class ShopController extends OGameController
      */
     public function index(): View
     {
+        //Bug Fix #418, Ryan Sandnes, 2021-08-02
+        $this->setBodyId('shop');
+
         return view('ingame.shop.index');
     }
 }
