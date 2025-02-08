e76dc3cb59387045e78160ab03a804c6c2bb1b7d
Ryan Sandnes <theshado40@hotmail.com>
2025-02-07 16:14:40 -0800

    Removed Signature from Shop Controller Removed Shop Page Test as Http200Test already exists

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -13,7 +13,6 @@ class ShopController extends OGameController
      */
     public function index(): View
     {
-        //Bug Fix #418, Ryan Sandnes, 2021-08-02
         $this->setBodyId('shop');
 
         return view('ingame.shop.index');
