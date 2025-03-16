fef1a3277bf28b929bda5b33a58188b4491a7fa7
Leendert de Borst <ldeborst@xivisoft.com>
2025-03-16 13:58:45 +0100

    Refactor techtree popup nav menu to a reusable component

--- resources/views/ingame/techtree/technology.blade.php
+++ resources/views/ingame/techtree/technology.blade.php
@@ -1,41 +1,17 @@
+<div id="technologytree" data-title="@lang('Technology') - {{ $object->title }}">
+    @include('ingame.techtree.partials.nav', ['currentAction' => 'technologies', 'objectId' => $object->id])
 
-<ul class="subsection_tabs">
-    <li>
-        <a class="overlay reiter"
-           data-overlay-same="true"
-           href="{{ route('techtree.ajax', ['tab' => 1, 'object_id' => $object->object->id]) }}">
-            <span>
-                Techtree            </span>
-        </a>
-    </li>
-    <li>
-        <a class="overlay reiter"
-           data-overlay-same="true"
-           href="{{ route('techtree.ajax', ['tab' => 4, 'object_id' => $object->object->id]) }}">
-            <span>
-                Applications            </span>
-        </a>
-    </li>
-    <li>
-        <a class="overlay reiter"
-           data-overlay-same="true"
-           href="{{ route('techtree.ajax', ['tab' => 2, 'object_id' => $object->object->id]) }}">
-            <span>
-                Techinfo            </span>
-        </a>
-    </li>
-    <li>
-        <a class="overlay reiter active"
-           data-overlay-same="true"
-           href="{{ route('techtree.ajax', ['tab' => 3, 'object_id' => $object->object->id]) }}">
-            <span>
-                Technology            </span>
-        </a>
-    </li>
-</ul>
+    <div class="techtree" data-id="5752c611e29741257f3cf67e060afb27" data-title="Applications - {{ $object->title }}">
+        <div class="advice">{{ $object->title }} is a prerequisite of:</div>
+        There are no such technologies.
+    </div>
 
-<div class="techtree" data-id="5752c611e29741257f3cf67e060afb27" data-title="Applications - {{ $object->object->title }}">
-    <div class="advice">{{ $object->object->title }} is a prerequisite of:</div>
-    There are no such technologies.        </div>
+</div>
+
 <script type="text/javascript">
-    $(document).ready(function(){initOverlayName();});</script>
+    $(
+        function(){
+            initOverlayName();
+        }
+    );
+</script>
