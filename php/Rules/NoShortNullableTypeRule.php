77a4564ab1456bdf8d52163c720988a73dfdf9df
Leendert de Borst <ldeborst@xivisoft.com>
2025-04-13 19:15:09 +0200

    Add custom PHPStan rule to check for short nullable type usage

---
+++ php/Rules/NoShortNullableTypeRule.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Php\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\FunctionLike;
+use PhpParser\Node\NullableType;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use PHPStan\Rules\IdentifierRuleError;
+
+/**
+ * Custom PHPStan rule that checks for short nullable type syntax, enforcing the use of `|null` instead of `?`.
+ * 
+ * @implements Rule<FunctionLike>
+ */
+final class NoShortNullableTypeRule implements Rule
+{
+    public function getNodeType(): string
+    {
+        return FunctionLike::class;
+    }
+
+    /**
+     * @param FunctionLike $node
+     * @return list<IdentifierRuleError>
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        $errors = [];
+
+        foreach ($node->getParams() as $param) {
+            if ($param->type instanceof NullableType) {
+                $errors[] = RuleErrorBuilder::message(sprintf(
+                    'Do not use shorthand "?%s". Use "%s|null" instead.',
+                    $param->type->type,
+                    $param->type->type
+                ))->line($param->getLine())->identifier('noShortNullableType')->build();
+            }
+        }
+
+        $returnType = $node->getReturnType();
+        if ($returnType instanceof NullableType) {
+            $type = $returnType->type;
+            $errors[] = RuleErrorBuilder::message(sprintf(
+                'Do not use shorthand "?%s". Use "%s|null" instead in return type.',
+                $type,
+                $type
+            ))->line($returnType->getLine())->identifier('noShortNullableType')->build();
+        }
+
+        return $errors;
+    }
+}
---
+++ php/Rules/README.md
@@ -0,0 +1,3 @@
+This folder contains custom rules for PhpStan to enforce certain code styles.
+
+These custom rules are included by the `phpstan.neon` file in the root of this project.
