35ccdfca55751804a8487478d59d7d434dcb024c
Leendert de Borst <ldeborst@xivisoft.com>
2025-07-21 23:31:53 +0200

    Include captured resources in expedition resource message

--- app/GameMessages/Expeditions/Abstracts/ExpeditionResourcesFoundGameMessage.php
+++ app/GameMessages/Expeditions/Abstracts/ExpeditionResourcesFoundGameMessage.php
@@ -13,11 +13,14 @@ abstract class ExpeditionResourcesFoundGameMessage extends ExpeditionGameMessage
     {
         $body = parent::getBody();
 
+        $params = parent::checkParams($this->message->params);
+        $params = parent::formatReservedParams($params);
+
         // Append the captured resource type and amount to the body if the params are set.
-        if (!empty($this->params['resource_type']) && !empty($this->params['resource_amount'])) {
+        if (!empty($params['resource_type']) && !empty($params['resource_amount'])) {
             // Convert resource type to human readable string.
-            $resourceTypeLabel = ResourceType::from($this->params['resource_type'])->getTranslation();
-            $body .= '<br /><br />' . __('t_messages.expedition_resources_captured', ['resource_type' => $resourceTypeLabel, 'resource_amount' => $this->params['resource_amount']]);
+            $resourceTypeLabel = ResourceType::from($params['resource_type'])->getTranslation();
+            $body .= '<br /><br />' . __('t_messages.expedition_resources_captured', ['resource_type' => $resourceTypeLabel, 'resource_amount' => $params['resource_amount']]);
         }
 
         return $body;
