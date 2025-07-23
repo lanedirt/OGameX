63924bf3c2268a93557fea11421d1c467d6d5ebe
Jack <jjbayliss@icloud.com>
2025-07-24 00:57:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import fs from 'fs';
+
+// We control the vite options rather than using the Laravel plugin, due to the fact we have a range scripts
+// within the blade files themselves, and using the Laravel plugin / directives mean the scripts become deferred
+// when we need them to load instantly (so the other blade scripts etc, can access the bits they need)
+// its very much anti how its designed, but prevents a re-write of everything. 
+// If you see a better way, feel free to PR!
+
+const outputPaths = [
+    'public/build/js/ingame.js',
+    'public/build/js/outgame.js',
+    'public/build/css/ingame.css',
+    'public/build/css/outgame.css',
+];
+
+// Pre-delete target files manually before build
+outputPaths.forEach(file => {
+    const fullPath = path.resolve(__dirname, file);
+    if (fs.existsSync(fullPath)) {
+        fs.unlinkSync(fullPath); // delete existing file
+    }
+});
 
 export default defineConfig({
     build: {
+        minify: false,
+        minifyIdentifiers: false,
+        outDir: 'public',
+        emptyOutDir: false, // we need to keep other files, but we manually delete just the needed ones above..
         rollupOptions: {
+            treeshake: false,
             input: {
                 ingame: path.resolve(__dirname, 'resources/js/ingame.js'),
                 outgame: path.resolve(__dirname, 'resources/js/outgame.js'),
@@ -11,18 +38,16 @@ export default defineConfig({
                 outgameStyle: path.resolve(__dirname, 'resources/css/outgame.css'),
             },
             output: {
-                inlineDynamicImports: false,
-                format: 'commonjs',
+                format: 'es',
                 entryFileNames: 'build/js/[name].js',
-                assetFileNames: (assetInfo) => {
-                    if (assetInfo.name.endsWith('.css')) {
+                assetFileNames: assetInfo => {
+                    if (assetInfo.name?.endsWith('.css')) {
                         return 'build/css/[name][extname]';
                     }
                     return 'build/assets/[name][extname]';
                 },
             },
+            
         },
-        outDir: 'public',
-        emptyOutDir: false,
-    }
+    },
 });
