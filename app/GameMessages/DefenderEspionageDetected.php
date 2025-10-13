22e37e7e4c7fb10f5d369df7b633df1cd7f5aa13
Geda173 <pkiss173@gmail.com>
2025-10-13 21:20:28 +0200

    Remove docblock
    PSR-12–clean version

--- app/GameMessages/DefenderEspionageDetected.php
+++ app/GameMessages/DefenderEspionageDetected.php
@@ -6,14 +6,9 @@ use OGame\GameMessages\Abstracts\GameMessage;
 
 class DefenderEspionageDetected extends GameMessage
 {
-    /**
-     * We only define the key, params, tab/subtab.
-     * Rendering (subject/body/from) will come from resources/lang/*/t_messages.php.
-     */
     protected function initialize(): void
     {
         $this->key    = 'espionage_detected';
-        // Matches the placeholders in the translation body below.
         $this->params = ['planet', 'coords', 'attacker_name', 'chance'];
         $this->tab    = 'fleets';
         $this->subtab = 'espionage';
