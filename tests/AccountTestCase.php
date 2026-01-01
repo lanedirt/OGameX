c72bc7df4d719d0e812c8a02bf70b09ad7fbc5da
Geda173 <pkiss173@gmail.com>
2026-01-01 11:46:09 +0100

    Import DB facade

--- tests/AccountTestCase.php
+++ tests/AccountTestCase.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Exception;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Testing\TestResponse;
 use OGame\Factories\GameMessageFactory;
