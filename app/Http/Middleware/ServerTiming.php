21cf849140f08ef9e7346b32577c48a5ed9cb5b8
Leendert de Borst <ldeborst@xivisoft.com>
2026-01-09 20:34:35 +0100

    Update ServerTiming.php

--- app/Http/Middleware/ServerTiming.php
+++ app/Http/Middleware/ServerTiming.php
@@ -4,7 +4,7 @@ namespace OGame\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 
 class ServerTiming
 {
@@ -20,8 +20,8 @@ class ServerTiming
         $response = $next($request);
 
         // Add server timing metrics for performance monitoring.
-        $processingTime = defined('LARAVEL_START') ? round((microtime(true) - LARAVEL_START) * 1000, 2) : 0;
-        $response->header('Server-Timing', 'app;dur=' . $processingTime . ', cdn;desc="miss", origin;desc="local"');
+        $processingTime = \defined('LARAVEL_START') ? round((microtime(true) - LARAVEL_START) * 1000, 2) : 0;
+        $response->headers->set('Server-Timing', 'app;dur=' . $processingTime . ', cdn;desc="miss", origin;desc="local"');
 
         return $response;
     }
