b222b4ca251025685ca633c4ef1ac88dfde35bda
Rouven <rbardtke@hotmail.com>
2026-01-13 20:59:36 +0100

    fix phpstan error

--- app/Http/Controllers/GalaxyController.php
+++ app/Http/Controllers/GalaxyController.php
@@ -12,6 +12,7 @@ use OGame\Factories\PlanetServiceFactory;
 use OGame\Models\Alliance;
 use OGame\Models\Enums\PlanetType;
 use OGame\Models\FleetMission;
+use OGame\Models\Highscore;
 use OGame\Models\Planet;
 use OGame\Models\Planet\Coordinate;
 use OGame\Services\BuddyService;
@@ -467,8 +468,9 @@ class GalaxyController extends OGameController
         $isTargetAdmin = $player->isAdmin();
 
         // Get player's highscore rank
+        /** @var Highscore|null $highscore */
         $highscore = $player->getUser()->highscore;
-        $playerRank = $highscore?->general_rank ?? null;
+        $playerRank = $highscore?->general_rank;
 
         // Calculate which page this player is on in the highscore (100 players per page)
         $highscorePage = $playerRank !== null ? (int)ceil($playerRank / 100) : 1;
