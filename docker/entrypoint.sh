a1823c2fdcab26b630f86d0701a5bf2842c50a96
Geda173 <pkiss173@gmail.com>
2026-02-25 16:25:13 +0100

    Change default port for Reverb in entrypoint.sh to match .env.example

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -29,7 +29,7 @@ if [ "$role" = "scheduler" ]; then
 elif [ "$role" = "queue" ]; then
       php /var/www/artisan queue:work --verbose --no-interaction
 elif [ "$role" = "reverb" ]; then
-    php /var/www/artisan reverb:start --host="${REVERB_SERVER_HOST:-0.0.0.0}" --port="${REVERB_SERVER_PORT:-8080}"
+    php /var/www/artisan reverb:start --host="${REVERB_SERVER_HOST:-0.0.0.0}" --port="${REVERB_SERVER_PORT:-8090}"
 elif [ "$role" = "app" ]; then
     # Check APP_ENV and run appropriate composer install
     if [ "$is_production" = true ]; then
