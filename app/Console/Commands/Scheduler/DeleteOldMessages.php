0c97c65d52e48e3ab95ba52fecdd00813139d61b
Geda173 <pkiss173@gmail.com>
2026-05-25 15:45:34 +0200

    feat: delete inbox messages after 7 days and archive chat messages after 7 days (#1423)
    * feat: add auto-deletion of messagesa fter 7 days
    
    * feat: delete inbox messages and archive chat messages older than 7 days

---
+++ app/Console/Commands/Scheduler/DeleteOldMessages.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OGame\Console\Commands\Scheduler;
+
+use Illuminate\Console\Command;
+use OGame\Models\ChatMessage;
+use OGame\Models\Message;
+
+class DeleteOldMessages extends Command
+{
+    private const RETENTION_DAYS = 7;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ogamex:scheduler:delete-old-messages';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes player inbox messages and archives chat messages older than the retention period.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $cutoff = now()->subDays(self::RETENTION_DAYS);
+        $deletedCount = 0;
+        $archivedCount = 0;
+
+        Message::where('created_at', '<=', $cutoff)
+            ->chunkById(1000, function ($messages) use (&$deletedCount): void {
+                $deletedCount += Message::whereIn('id', $messages->modelKeys())->delete();
+            });
+
+        ChatMessage::where('created_at', '<=', $cutoff)
+            ->chunkById(1000, function ($messages) use (&$archivedCount): void {
+                $archivedCount += ChatMessage::whereIn('id', $messages->modelKeys())->delete();
+            });
+
+        $this->info("Deleted {$deletedCount} inbox message(s) older than " . self::RETENTION_DAYS . ' days.');
+        $this->info("Archived {$archivedCount} chat message(s) older than " . self::RETENTION_DAYS . ' days.');
+
+        return Command::SUCCESS;
+    }
+}
